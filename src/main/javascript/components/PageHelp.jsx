dbbdb9e30d3c0108dcf5d1cebc5af46ab3e2cb08
headzoo <sean@headzoo.io>
2017-11-07 20:24:48 -0500

    Initial commit

---
+++ src/main/javascript/components/PageHelp.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { sdkConnect, LinkButton } from '@deskpro/apps-sdk-react';
+import { Container, Section, Heading } from '@deskpro/react-components';
+
+/**
+ * Renders the app's help page.
+ */
+const PageHelp = () => (
+  <Container>
+    <Heading size={2}>
+      Help
+    </Heading>
+    <Section>
+      <p>
+        Use this app to render details of the opened tab using
+        a custom HTML template.
+      </p>
+
+      <Heading size={3}>
+        Template
+      </Heading>
+      <p>
+        The tab data is displayed using an admin created template which may
+        contain <a href="http://handlebarsjs.com/expressions.html" target="_blank">Handlebars expressions</a>.
+      </p>
+      <pre>
+        <code>
+          {`<div>
+  Ticket by {{person.emails.[0].email}}
+</div>`}
+        </code>
+      </pre>
+    </Section>
+    <LinkButton to="home">
+      Close
+    </LinkButton>
+  </Container>
+);
+
+export default sdkConnect(PageHelp);
---
+++ src/main/javascript/components/PageHome.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Handlebars from 'handlebars';
+import { sdkConnect } from '@deskpro/apps-sdk-react';
+import { Container } from '@deskpro/react-components';
+
+/**
+ * Renders the defined template.
+ */
+class PageHome extends React.PureComponent {
+  static propTypes = {
+    /**
+     * Instance of sdk storage.
+     * @see https://deskpro.gitbooks.io/deskpro-apps/api/props/storage.html
+     */
+    storage: PropTypes.object.isRequired,
+
+    /**
+     * Populated with the details of the currently opened ticket.
+     * @see https://deskpro.gitbooks.io/deskpro-apps/api/props/tabdata.html
+     */
+    tabData: PropTypes.object.isRequired,
+
+    /**
+     * Instance of sdk ui.
+     * @see https://deskpro.gitbooks.io/deskpro-apps/api/props/ui.html
+     */
+    ui: PropTypes.object.isRequired
+  };
+
+  /**
+   * Constructor
+   *
+   * @param {*} props
+   */
+  constructor(props) {
+    super(props);
+    try {
+      const template = Handlebars.compile(props.storage.app.settings.template || '');
+      this.html = template(props.tabData);
+    } catch (error) {
+      props.ui.error(error);
+      props.route.to('settings');
+    }
+  }
+
+  /**
+   * @returns {XML}
+   */
+  render() {
+    return (
+      <Container>
+        <div dangerouslySetInnerHTML={{ __html: this.html }} />
+      </Container>
+    );
+  }
+}
+
+export default sdkConnect(PageHome);
